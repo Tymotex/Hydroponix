bfc0a3aa055568012cba5509981a8b7acc4e4915
Tim Zhang <54927071+Tymotex@users.noreply.github.com>
2019-12-26 22:50:56 +1100

    Update 1
    Currently unimplemented:
    - GetLastWatered 
    - Enable/disable watering

--- water.py
+++ water.py
@@ -1,11 +1,16 @@
 import RPi.GPIO as GPIO
+import Adafruit_DHT
 from time import sleep
+
+isEnabled = True
 
 # Pins
 relayTrigger = 21
 moistureSensor = 20
-statusLED = 16   
+statusLED = 16
+tempHumidSensor = 4
 
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(relayTrigger, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(moistureSensor, GPIO.IN)
@@ -17,15 +22,41 @@ def on(pin):
 def off(pin):
     GPIO.output(pin, GPIO.LOW)
 
+def GetHumidityAndTemp():
+	humidity, temperature = Adafruit_DHT.read(Adafruit_DHT.AM2302, tempHumidSensor)
+	if humidity is not None and temperature is not None:
+		humidAndTemp = {
+			'humidity' : humidity,
+			'temperature' : temperature
+		}
+		return humidAndTemp
+	else:
+		# On failure, return a dictionary with zeroed values
+		# TODO: Need better error-handling
+		return {
+			'humidity' : 0,
+			'temperature' : 0
+		}
+
 if __name__ == '__main__':
-    while True:
-        print("=== Looping ===")
-        if GPIO.input(moistureSensor):
-            print("Dry")
-            relayOff(relayTrigger)
-            off(statusLED)
-        else:
-            print("Wet")
-            relayOn(relayTrigger)
-            on(statusLED)
-        sleep(1)
+	while True:
+		print("=== Looping ===")
+		if GPIO.input(moistureSensor):
+			print("Dry")
+			off(relayTrigger)
+			off(statusLED)
+		else:
+			print("Wet")
+			on(relayTrigger)
+			on(statusLED)
+		
+		data = GetHumidityAndTemp()
+		print('Temp={0:0.1f}°C  Humidity={1:0.1f}%'.format(data["temperature"], data["humidity"]))	
+		
+		sleep(1)
+
+
+
+def GetLastWatered():
+	print("===> Getting last watered")
+	return "Never!"
